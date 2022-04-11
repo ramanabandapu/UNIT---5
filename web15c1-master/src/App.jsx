c7c5d8292e96551fed58ac1f36aaaa66e2d4078f
ramanabandapu <bandapu.ramana@gmail.com>
2022-04-11 17:28:58 +0530

    donbe

---
+++ web15c1-master/src/App.jsx
@@ -0,0 +1,89 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import  "./App.css";
+
+
+function App() {
+
+const [counter, setCounter] = useState(76);
+
+const handleChange = (value) => {
+
+
+  setCounter(counter + value);
+}
+
+
+
+  return (
+    <div className="App">
+      <h3>India:</h3>
+      <div className="banner">
+        <div>
+          Score:{" "}
+          <h1 className="scoreCount">
+            {
+              // show "score" here
+            }
+          </h1>
+        </div>
+        <div>
+          Wicket:{" "}
+          <h1 className="wicketCount">
+            {
+              // show wicket here
+            }
+          </h1>
+        </div>
+
+        <div>
+          Over:{" "}
+          <h1 className="overCount">
+            {
+              // Show Over here in the format: "over.ball" eg: 4.5 means 4th over and 5th ball
+              // if 1 more ball is thrown then over is now 5.0
+              // you have to write logic to form this string from current ball number.
+            }
+          </h1>
+        </div>
+      </div>
+
+      <div className="addScore">
+        Add Score
+        {/* these buttons should add the respective amount in the score */}
+        <button className="addScore1" onClick={() => handleChange(1)}>Add 1</button>
+        <button className="addScore4"  onClick={() => handleChange(4)}>Add 4</button>
+        <button className="addScore6"  onClick={() => handleChange(6)}>Add 6</button>
+      </div>
+
+      <div className="addWicket">
+        Add Wicket
+        {/* Increase the count of wicket */}
+        <button  onClick={() => handleChange(1)}>Add 1 wicket</button>
+      </div>
+
+      <div className="addBall">
+        Add ball
+        {/* Increase the total number of balls thrown here. */}
+        <button  onClick={() => handleChange(1)}>Add 1</button>
+      </div>
+
+         Score: {Score > 100 ? "India Won" : null}   
+
+
+
+      {/* If score reaches greater than 100, show text "India Won" without quotes in h1 tag with class name 'status' */}
+    </div>
+  );
+}
+ReactDOM.render(
+
+
+  document.getElementById("root");
+
+root.appendChild(App)
+
+);
+export default App;
